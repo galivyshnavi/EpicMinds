6fef657d01f24668297209864cd106d779499058
galivyshnavi <166641910+galivyshnavi@users.noreply.github.com>
2025-09-13 13:47:19 +0530

    Update App.jsx

--- admin_panel/src/App.jsx
+++ admin_panel/src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 
 import LoginPage from './Components/Login/Login'
 import './App.css'
+import Layout from './Components/Sample/Layout'
 
 function App() {
   return(
     <LoginPage/>
+   
   )
 }
 
